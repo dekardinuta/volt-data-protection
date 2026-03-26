8773f6317f754063ea5f9ddb9057a3f27db8476b
Carlos G. Mogollón A. <152676014+dekardinuta@users.noreply.github.com>
2026-03-26 19:22:57 -0400

    fix: vincular formulario con la nueva url operativa

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 // 1. La URL de tu motor de ingeniería en Google
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzDLo1IfmuTL7IOkr1zTwjyYmMp9JVA0i4h6Kuj8b7ia_rJybUzagmFjFSpW5Tppb7JIg/exec";
+const WEB_APP_URL = "https://script.google.com/macros/s/***REMOVED***/exec";
 
 document.getElementById('lead-form').addEventListener('submit', function(e) {
     e.preventDefault(); 
